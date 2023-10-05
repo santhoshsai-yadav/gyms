6a2d912b5740ed89127b83e081c61b2944ddfbad
saisanthosh yadav <141811579+santhoshsai-yadav@users.noreply.github.com>
2023-10-05 11:17:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// let nav =document.querySelector(".navbar");
+// window.onscroll=function(){
+//     if(document.documentElement.scrollTop>20){
+//         nav.classList.add("header.scrolld");
+//     }
+//     else{
+//         nav.classList.remove("header-scrolled");
+//     }  
+// }
